0f87342043a339118f211056d37f54cfe1237d92
KrishK060 <kalariakrish0601@gmail.com>
2025-02-06 20:11:30 +0530

    added a functionality such that same category cards comes next to each other

--- js/flipkart.js
+++ js/flipkart.js
@@ -1,30 +1,11 @@
-const selectedCategory = JSON.parse(localStorage.getItem('category')) || [];;
+const selectedCategory = JSON.parse(localStorage.getItem('category')) || [];
 const products = JSON.parse(localStorage.getItem('crud')) || [];
-console.log(selectedCategory.name)
-for (let i in selectedCategory) {
-    let exists = 0;
-    for (let j in products) {
-        if (i.name == products[j]['category']) {
-            let exists = 1
-            if (exists) {
-                const div = document.createElement('div');
-                div.classList.add('container bg-light mt-3 p-2')
-                div.innerHTML = `
-            <div class="initial-items-list">
-                <div class="row">
-                    <div class="d-flex">
-                         <div class="cards-wrapper">
-                         </div>
-                    </div>
-                </div>
-            </div>
-                `
-            }
-        }
-    }
-}
+
+const productList = document.getElementById('accessories-section');
+productList.innerHTML = '';
 
 
+let categoryRows = {};
 
 let filteredProducts = [];
 
@@ -33,47 +14,53 @@ products.forEach((product) => {
         if (product.category == scategory.name) {
             filteredProducts.push(product);
         }
-    })
-})
+    });
+});
 
-
-
-const productList = document.getElementById('accessories-section');
-
-
-productList.innerHTML = '';
 
 if (filteredProducts.length === 0) {
     productList.innerHTML = '<p>No products available in this category.</p>';
-   
 }
 
 
 filteredProducts.forEach(product => {
-    const col1 = document.createElement('div');
-    col1.classList.add('row-ms-2')
-    col1.innerHTML = `
-        <h4>${product.category}</h4>
-        `
+    
+    if (!categoryRows[product.category]) {
+    
+        const categoryContainer = document.createElement('div');
+        categoryContainer.classList.add('container', 'bg-light', 'mt-3', 'p-2');
+
+
+        const categoryHeading = document.createElement('h4');
+        categoryHeading.textContent = product.category;
+        categoryContainer.appendChild(categoryHeading);
+
+    
+        const categoryRow = document.createElement('div');
+        categoryRow.classList.add('row', 'g-3'); 
+        categoryRow.setAttribute('id', `row-${product.category}`);
+
+        categoryContainer.appendChild(categoryRow);
+        productList.appendChild(categoryContainer);
+
+        
+        categoryRows[product.category] = categoryRow;
+    }
+
+    
     const col = document.createElement('div');
-   
-    col.classList.add('col-md-4');
+    col.classList.add('col-md-4', 'd-flex'); 
 
     col.innerHTML = `
-            <div class="card">
-                <img src="${product.img}" class="card-img-top" alt="${product.name}">
-                <div class="card-body">
-                    <h5 class="card-title">${product.name}</h5>
-                  
-                    <p class="card-text">$${product.price}</p>
-                    
-                </div>
+        <div class="card w-100">
+            <img src="${product.img}" class="card-img-top" alt="${product.name}">
+            <div class="card-body">
+                <h5 class="card-title">${product.name}</h5>
+                <p class="card-text">$${product.price}</p>
             </div>
-        `;
-    productList.appendChild(col1);
-    productList.appendChild(col);
+        </div>
+    `;
 
-
+    
+    categoryRows[product.category].appendChild(col);
 });
-
-
